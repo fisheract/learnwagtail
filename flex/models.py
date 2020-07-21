cf225d0f494351067cb19577ab7e7ea4c8a9f0aa
fisheract <geremy85@gmail.com>
2020-07-21 07:23:50 +0600

    first commit

---
+++ flex/models.py
@@ -0,0 +1,24 @@
+"""Flexible page"""
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.models import Page
+
+
+class FlexPage(Page):
+    """Flexible page class"""
+
+    template = "flex/flex_page.html"
+
+    # @to-do add stream fields
+    # content = StreamField()
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle")
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
